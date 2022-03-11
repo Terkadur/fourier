11eb85fab2c72dbfcab3ae742d2d651997f00da2
Terkadur <47321551+Terkadur@users.noreply.github.com>
2022-03-10 22:42:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ let alph_sloider;
 let cpt_sloider;
 
 function setup() {
-  print("Version 1.3");
+  print("Version 1.4");
   document.body.style.overflow = 'hidden';
   let width1 = windowWidth;
   let width2 = (windowHeight - 4)/0.4;
